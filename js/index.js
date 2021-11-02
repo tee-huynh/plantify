7addf4cc7606c49e79a036f9d2a3f41b0b55aa9b
Tina Huynh <47578470+tee-huynh@users.noreply.github.com>
2021-11-02 15:43:46 -0600

    testing with streaming data

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 // set the dimensions and margins of the graph
 const margin = {top: 10, right: 10, bottom: 10, left: 10},
-    width = 900 - margin.left - margin.right,
-    height = 900 - margin.top - margin.bottom,
+    width = 1200 - margin.left - margin.right,
+    height = 1200 - margin.top - margin.bottom,
     innerRadius = 80,
     outerRadius = Math.min(width, height) / 2;   // the outerRadius goes from the middle of the SVG area to the border
 
@@ -13,13 +13,13 @@ const svg = d3.select("#my_dataviz")
   .append("g")
     .attr("transform", `translate(${width/2},${height/2+100})`); // Add 100 on Y translation, cause upper bars are longer
 
-d3.json("https://raw.githubusercontent.com/tee-huynh/spotifycts/main/js/plays.csv").then( function(data) {
+d3.json("https://raw.githubusercontent.com/tee-huynh/spotifycts/main/js/StreamingHistory0.json").then( function(data) {
 
   // X scale
   const x = d3.scaleBand()
       .range([0, 2 * Math.PI])    // X axis goes from 0 to 2pi = all around the circle. If I stop at 1Pi, it will be around a half circle
       .align(0)                  // This does nothing ?
-      .domain( data.map(d => d.Country)); // The domain of the X axis is the list of states.
+      .domain( data.map(d => d.artistName)); // The domain of the X axis is the list of states.
 
   // Y scale
   const y = d3.scaleRadial()
@@ -34,9 +34,9 @@ d3.json("https://raw.githubusercontent.com/tee-huynh/spotifycts/main/js/plays.cs
       .attr("fill", "#1ed760")
       .attr("d", d3.arc()     // imagine your doing a part of a donut plot
           .innerRadius(innerRadius)
-          .outerRadius(d => y(d['Value']))
-          .startAngle(d => x(d.Country))
-          .endAngle(d => x(d.Country) + x.bandwidth())
+          .outerRadius(d => y(d['msPlayed']))
+          .startAngle(d => x(d.artistName))
+          .endAngle(d => x(d.artistName) + x.bandwidth())
           .padAngle(0.01)
           .padRadius(innerRadius))
 
